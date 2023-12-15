821a1df4a1e01dc065e52f47d84a714cee485ac6
REDMIBOOK <REDMIBOOK@LAPTOP-CU9HA562.lan>
2023-12-15 21:46:13 +0700

    test rodjil

--- src/Ecommerce.java
+++ src/Ecommerce.java
@@ -14,6 +14,8 @@ public class Ecommerce {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //sagsagsjagsjagsjagsagjs
+        System.out.println("asjdjabdjabdja");
     }
     
 }
