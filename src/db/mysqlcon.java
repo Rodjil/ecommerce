b9655f3064b785952bf008138d0d591ecdaf1935
Olionnn <cendekiaid2019@gmail.com>
2023-12-17 10:40:35 -0800

    add class mysql dv

--- src/db/mysqlcon.java
+++ src/db/mysqlcon.java
@@ -5,40 +5,42 @@ import java.sql.*;
 
 
 public class mysqlcon {
-    public static void main(String arg[]) throws ClassNotFoundException{
-        try{
-            Connection conn = null;
-
-            String MySqlUser ="root", MySqlPassword = "121212";
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/ecom-db", 
-                MySqlUser, 
-                MySqlPassword);
-
-            Statement query;
-            query = conn.createStatement();
-            ResultSet rs;
-            rs = query.executeQuery(
-                "select * from user"
-            );
-            int userId;
-            String userName;
-
-            while (rs.next()) {
-                userId = rs.getInt("user_id");
-                userName = rs.getString("user_name");
-                System.out.println("User :" + userId + "\n"+ "Name :" + userName);
-                rs.close();
-                query.close();
-                conn.close();
-            }
 
 
+    private static String MYSQL_JDBC_DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+    private static String MYSQL_DB_URL = "jdbc:mysql://localhost:3306/ecom-db";
+    private static String MYSQL_DB_USER = "root";
+    private static String MYSQL_DB_USER_PASSWORD = "121212";
+    private static String SQL_QUERY = "SELECT DATABASE() FROM DUAL";
 
-        }catch (SQLException e) {
-            System.out.println(e);
-        }
+    public static void connect() {
+            
+        try(Connection connection = DriverManager.getConnection(MYSQL_DB_URL,MYSQL_DB_USER,MYSQL_DB_USER_PASSWORD)) {
+
+
+            // CARA TAMBAH DRIVER MYSQL : 
+            // VSCODE : ctrl + shift + p > java classpath > tambah jar driver
+            // NETBEANS : lupa
+            Class.forName(MYSQL_JDBC_DRIVER_CLASS); 
+            Statement statement =connection.createStatement(); 
+            
+            
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY); 
+
+            while(resultSet.next())  {
+                if (resultSet.getString(1).equals("ecom-db") ) {
+                    System.out.println("Database Connected...");
+                } 
+            }     
+                
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Driver class not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error occured while executing query: ");
+
+            e.printStackTrace();
+        } 
     }
+
 }
